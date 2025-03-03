d8e504815fa1d229ef0bd04d97f4b142a871b4b0
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-03 19:32:33 +0100

    Fix Animale class

--- src/org/lessons/java/abst/interf/Animale.java
+++ src/org/lessons/java/abst/interf/Animale.java
@@ -2,11 +2,6 @@ package org.lessons.java.abst.interf;
 
 public abstract class Animale {
 
-    public Animale() {
-
-    }
-
-
     public void dormi(){
         System.out.println("Zzzz");
     };
